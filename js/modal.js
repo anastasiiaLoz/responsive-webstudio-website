9bfbba09d50562b1555d3d402c76ce8cd67a5e3d
Anastasiia Lozovych <anna.collana@gmail.com>
2021-03-03 20:08:14 +0000

    Добавляет правки по секциям

--- js/modal.js
+++ js/modal.js
@@ -2,15 +2,27 @@ console.log('123');
 
 (() => {
     const refs = {
+    openModalBurgerBtn: document.querySelector('[data-modal-burger-open]'),
+    closeModalBurgerBtn: document.querySelector('[data-modal-burger-close]'),
+    modalBurger: document.querySelector('[data-modal-burger]'),
+
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     };
   
+    refs.openModalBurgerBtn.addEventListener('click', toggleModalBurger);
+    refs.closeModalBurgerBtn.addEventListener('click', toggleModalBurger);
+
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
-    function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    function toggleModalBurger() {
+    refs.modalBurger.classList.toggle('is-hidden');
     }
+
+    function toggleModal() {
+  
+      refs.modal.classList.toggle('is-hidden');
+      }
   })();
